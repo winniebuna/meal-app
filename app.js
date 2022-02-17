1e935c286af26f2fec7259fc28ef401779c2f882
winniexbuna <obunaborc@gmail.com>
2022-02-17 12:52:53 +0100

    Added the animation for the modal

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const modalBtn = document.querySelector('.check-em');
 const modalMenu = document.querySelector('.modal-menu');
+const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close-menu')
-
+const image = document.querySelector('.image-div');
 modalBtn.addEventListener('click', function(){
-    modalMenu.classList.add('bg-menu')
+    modalMenu.classList.add('bg-menu');
+    modal.classList.add('bg-menu2')
 });
 closeBtn.addEventListener('click', function(){
-    modalMenu.classList.remove('bg-menu')
+    modalMenu.classList.remove('bg-menu');
+    modal.classList.remove('bg-menu2');
+    // modal.classList.add('modal-rev');
 });
+image.addEventListener('click', () => {
+    image.classList.add("stuff");
+})
